4dd96152dc562f667adec265c87c225f14fdd288
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-28 09:25:38 +1200

    still trying to fix images but finished history page

--- website/app.py
+++ website/app.py
@@ -71,6 +71,10 @@ def all_designers():
     conn.close()
     return render_template('designers.html', designers=designers)
 
+@app.route('/history')
+def fashion_history():
+    return render_template("history.html")
+
 if __name__ == '__main__':
     init_db()  # Initialize database tables
     app.run(debug=True)
